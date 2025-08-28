2a18a5b6d03daecea872dc8178196d8d8013f04c
Debbie-alt <funmmy565@gmail.com>
2025-08-28 18:29:01 +0100

    Fix Dog class initialization and improve method outputs

--- oop.py
+++ oop.py
@@ -2,3 +2,8 @@ class Dog():
     def __init__(self, breed, size):
         self.breed = breed
         self.size = size
+        print("THe dog's name is {}".format(self.name))
+    def eat(self, food):
+        print("The {} bread loves eba {}".format(self.breed, food))
+    def sleep_time(self, time):
+        print("The {} dog loves to sleep at 6".format(self.breed))        
